cce9601081f1b92107c113c5b96c2c07b8f8c21b
sam neat <samjneat@gmail.com>
2022-07-14 14:53:23 +0100

    snake can eat food and expands when food is eaten

--- snake.js
+++ snake.js
@@ -1,9 +1,11 @@
 import { getInputDirection } from "./input.js"
 
-export const SNAKE_SPEED = 1
+export const SNAKE_SPEED = 5
 const snakeBody = [{ x: 11, y: 11 }]
+let newSegments = 0
 
 export function update() {
+  addSegments()
   const inputDirection = getInputDirection()
   for (let i = snakeBody.length -2; i >= 0; i --) {
     snakeBody[i + 1] = { ...snakeBody[i] }
@@ -20,4 +22,24 @@ export function draw(gameBoard) {
     snakeElement.classList.add('snake')
     gameBoard.appendChild(snakeElement)
   })
+}
+
+export function expandSnake(amount) {
+  newSegments += amount
+}
+
+export function onSnake(position) {
+  return snakeBody.some(segment => {
+    return equalPositions(segment, position)
+  })
+}
+
+function equalPositions(pos1, pos2) {
+  return pos1.x === pos2.x && pos1.y === pos2.y
+}
+
+function addSegments() {
+  for (let i =0; i < newSegments; i ++) {
+    snakeBody.push({ ...snakeBody[snakeBody.length - 1]})
+  }
 }
